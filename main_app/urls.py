0d76b0921145c88440220d7ec9e9112845a2b081
Alex Keller <kelleralex04@gmail.com>
2023-12-31 21:41:03 -0700

    adds calendar functionality and routing to detail page for each day

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('calendar/', views.calendar, name='calendar'),
+    path('calendar/', views.calendarIndex, name='calendar'),
+    path('calendar/m<int:curMonth>d<int:i>y<int:curYear>', views.calendarDetail, name='day'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+import calendar
+
+date = datetime.now()
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+days = [1, 2, 3, 4, 5, 6, 0]
+curDay = date.day
+curMonth = date.month
+curYear = date.year
+todayDay = curDay
+todayMonth = curMonth
+todayYear = curYear
 
 def home(request):
     return render(request, 'home.html')
 
-def calendar(request):
-    return render(request, 'calendar.html')
+def calendarIndex(request):
+    global curMonth, curYear
+    if request.POST:
+        if 'prev' in request.POST:
+            curMonth -= 1
+        else:
+            curMonth += 1
+
+    if curMonth > 12:
+        curYear += 1
+        curMonth = 1
+    elif curMonth < 1:
+        curYear -= 1
+        curMonth = 12
+
+    firstDayOfMonth = days[datetime(curYear, curMonth, 1).weekday()]
+    lastDateofMonth = calendar.monthrange(curYear, curMonth)
+    lastDayOfMonth = days[datetime(curYear, curMonth, lastDateofMonth[1]).weekday()]
+    lastDateOfPrevMonth = (datetime(curYear, curMonth, 1) - timedelta(days = 1)).day
+    firstInactiveDays = []
+    activeDays = []
+    lastInactiveDays = []
+
+    for i in range(firstDayOfMonth, 0, -1):
+        firstInactiveDays.append(lastDateOfPrevMonth + 1 - i)
+
+    for i in range(lastDateofMonth[1]):
+        activeDays.append(i + 1)
+
+    if lastDayOfMonth < 7:
+        for i in range(6 - lastDayOfMonth):
+            lastInactiveDays.append(i + 1)
+
+    curMonthText = months[curMonth - 1]
+
+    return render(request, 'calendar.html', {
+        'curMonthText': curMonthText, 'curYear': curYear, 'curMonth': curMonth, 'curDay': curDay, 'firstInactiveDays': firstInactiveDays, 'activeDays': activeDays, 
+        'lastDateOfPrevMonth': lastDateOfPrevMonth, 'lastInactiveDays': lastInactiveDays, 'todayDay': todayDay, 'todayMonth': todayMonth, 'todayYear': todayYear 
+    })
+
+def calendarDetail(request, curMonth, i, curYear):
+    month = months[curMonth - 1]
+    return render(request, 'day.html', {
+        'curMonth': month, 'curDay': i, 'curYear': curYear
+    })

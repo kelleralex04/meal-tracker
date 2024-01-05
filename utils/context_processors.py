f7471743d8680a661be621866998e626788a7684
Alex Keller <kelleralex04@gmail.com>
2024-01-05 10:39:50 -0700

    fixes user authentication for profile login

---
+++ utils/context_processors.py
@@ -0,0 +1,15 @@
+from main_app.models import Profile
+from django.contrib.auth.models import User
+
+
+def profile_context(request):
+    test = None
+    if request.user.id:
+        user = User.objects.get(id=request.user.id)
+        profile = Profile.objects.filter(user=user)
+        if len(profile):
+            print(profile)
+            return {'profile': profile[0]}
+        else: return {'test': test}
+    else:
+        return {'test': test}
